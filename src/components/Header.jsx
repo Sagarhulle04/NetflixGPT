6d9a4a8c08d7030bc139b04a8bef2a7561b1b07c
Sagar Hulle <sagarhulle22@gmail.com>
2026-04-02 16:49:19 +0530

    change the logo image and the favicon image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div className=" px-10 z-10 bg-linear-to-b from-black">
       <Link to={logoTo}>
         <img
-          src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-03-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          src="https://i.postimg.cc/85X6Zn9S/streamora.png"
           alt="netflix-logo"
           className="h-20 w-60"
         />
