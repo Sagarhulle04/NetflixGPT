d2ffd64f7bf8630b18054f96bd3f83b83b6160ff
Sagar Hulle <sagarhulle22@gmail.com>
2026-04-03 14:49:47 +0530

    changed some code beacuse it was not running on the mobile

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -47,7 +47,7 @@ export default function VideoBackground({ movie }) {
           <div className="absolute top-0 left-0 w-screen aspect-video overflow-hidden">
             <iframe
               className="absolute top-1/2 left-1/2 min-w-[177.78%] min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 pointer-events-none scale-150"
-              src={`https://www.youtube.com/embed/${videoKey.key}?playlist=${videoKey.key}&loop=1&autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&showinfo=0&vq=hd1080`}
+              src={`https://www.youtube.com/embed/${videoKey.key}?playlist=${videoKey.key}&loop=1&autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&showinfo=0&playsinline=1&vq=hd1080`}
               title="background-video"
               allow="autoplay; encrypted-media; picture-in-picture"
             />
